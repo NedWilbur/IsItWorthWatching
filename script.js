ea1c572ce6134c4c761de06d1ec0922bcbcf2dde
NedWilbur <Ned612@Gmail.com>
2020-10-21 22:59:32 -0400

    Foundation set

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function () {
+    var search = document.querySelector('#search');
+    var result = document.querySelector('#result');
+
+    // create search event listener
+    search.addEventListener('input', inputChange);
+    resizeInput.call(search);
+})
+
+function inputChange() {
+    var length = this.value.length;
+
+    // resize field
+    this.style.width = length + "ch";
+
+    // set result
+    if (length > 1)
+        setResult(true);
+    else
+        result.innerHTML = null;
+}
+
+function setResult(watch) {
+    if (watch)
+        result.innerHTML = "yes.";
+    else
+        result.innerHTML = "no.";
+}
