6e1a3b8e1fa400a5f12052ba1ee4cd0b3db1aa84
NedWilbur <Ned612@Gmail.com>
2020-10-29 23:34:41 -0400

    Search POST added

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ const app = Express()
 const PORT = process.env.PORT || 5000
 const __dirname = Path.resolve(Path.dirname(''));
 
+// body parser middleware (required for post)
+app.use(Express.json());
+app.use(Express.urlencoded({
+    extended: false
+}));
+
 // set static folder
 app.use(Express.static(Path.join(__dirname, 'public')))
 
 // point all routes to client front end (built version)
 app.get('/', (req, res) => res.sendFile(Path.join(__dirname, 'public/index.html')));
 
+// ./title
 app.get('/:title', async (req, res) => {
     res.send(await Movies.GetMovie(req.params.title));
+});
+
+// form search
+app.post('/search', async (req, res) => {
+    const query = req.body.query;
+    res.send(await Movies.GetMovie(query));
 });
 
 // run server
