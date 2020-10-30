4554f18c562e558023cd0027b17c421c07b2d66a
NedWilbur <Ned612@Gmail.com>
2020-10-30 14:58:54 -0400

    Search results selectable

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const ratingFilter = 7;
+var queryResults = null;
 
 window.addEventListener('load', function () {
     var search = document.querySelector('#search');
@@ -6,43 +7,63 @@ window.addEventListener('load', function () {
     var result = document.querySelector('#result');
 
     // create search event listener
-    search.addEventListener('input', inputChange);
-    inputChange.call(search);
+    search.addEventListener('input', NewQuery);
+    ResizeSearchInput.call(search);
 })
 
-function inputChange() {
-    var length = this.value.length;
-    if (length <= 1) {
+function NewQuery() {
+    ResizeSearchInput();
+
+    // clear results
+    if (search.value.length <= 1) {
         result.innerHTML = null;
-        options.innerHTML = null; // clear current results
+        options.innerHTML = null;
         return;
     }
 
-    // resize field
-    this.style.width = length + "ch";
+    SendRequest(search.value);
+}
 
+function ResizeSearchInput() {
+    search.style.width = search.value.length + "ch";
+}
+
+function SendRequest(query) {
     // send request
     var req = new XMLHttpRequest();
-    req.open('GET', `/${this.value}`);
+    req.open('GET', `/${query}`);
     req.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            // request results
-            var res = JSON.parse(this.response);
-
-            options.innerHTML = null; // clear current results
-            res.forEach(movie => {
-                options.innerHTML += movie.title;
-                options.innerHTML += "</br>";
-            });
-
-            movie.vote_average >= ratingFilter ? setResult(true, res.title) : setResult(false, res.title);
-        }
+        if (this.readyState == 4 && this.status == 200)
+            HandleQueryResults(JSON.parse(this.response));
     }
     req.send();
 }
 
-function setResult(worthWatching, title) {
-    if (worthWatching)
+function HandleQueryResults(queryResults) {
+    options.innerHTML = null; // clear current results
+    queryResults.forEach(movie => {
+        var newElement = document.createElement('p');
+        newElement.setAttribute('title', movie.title);
+        newElement.setAttribute('rating', movie.vote_average);
+        newElement.innerHTML = movie.title;
+        newElement.addEventListener('click', OptionSelected);
+        options.appendChild(newElement); // add to options list
+
+
+        // options.innerHTML += `<button type="button" id=${movie.id} title="${movie.title}" rating="${movie.vote_average}">`${movie.title}</button></br>`; // add to doc
+        // document.getElementById(movie.id).addEventListener('click', OptionSelected);
+    }); // create listener
+};
+
+function OptionSelected() {
+    options.innerHTML = null; // clear options
+    search.value = this.getAttribute('title'); // set search option
+    ResizeSearchInput();
+    SetResult(this.getAttribute('rating'));
+}
+
+function SetResult(rating, title) {
+    if (rating >= ratingFilter)
         result.innerHTML = title + "</br>Yes.";
     else
         result.innerHTML = title + "</br>No.";
