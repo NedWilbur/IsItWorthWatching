75429de41ae9eeaeea2070cad13d1395a45ab09f
NedWilbur <Ned612@Gmail.com>
2020-10-29 23:12:34 -0400

    Cleaned API get movie

--- Movies.js
+++ Movies.js
@@ -3,40 +3,19 @@ import {
 } from 'moviedb-promise'
 const moviedb = new MovieDb('9b0b73c0081cd92f6455b2bace97c1ca')
 
-// example json
-const all = [{
-    id: 1,
-    name: 'Shrek',
-    rating: 99,
-}, {
-    id: 2,
-    name: 'Bambi',
-    rating: 59,
-}, {
-    id: 3,
-    name: 'Frozen',
-    rating: 32,
-}];
+async function GetMovie(title) {
+    console.log(`Searching for ${title}`);
 
-const findMovie = async title => {
-    // Equivalant to { query: title }
-    // const res = await moviedb.searchMovie(title)
-
-    // return res
-}
-
-// get 
-// 808 = Shrek
-function GetMovie(title) {
-    moviedb.searchMovie({
+    const result = await moviedb.searchMovie({
         query: title
     }).then(res => {
-        console.log(res)
-        return res;
+        console.log(`Results returned: ${res}`);
+        return res.results[0];
     }).catch(console.error)
+
+    return result;
 }
 
 export default {
-    all,
     GetMovie
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Express from 'express';
 import Path from 'path';
-import movies from './Movies.js';
+import Movies from './Movies.js';
 
 const app = Express()
 const PORT = process.env.PORT || 5000
@@ -12,18 +12,8 @@ app.use(Express.static(Path.join(__dirname, 'public')))
 // point all routes to client front end (built version)
 app.get('/', (req, res) => res.sendFile(Path.join(__dirname, 'public/index.html')));
 
-// API routes
-app.get('/api', (req, res) => {
-    console.log(`Sending all`)
-    npm
-    res.json(movies.all);
-});
-
-app.get('/api/test', (req, res) => {
-    var id = req.params.id;
-    console.log(`Running Test`);
-
-    res.json(movies.GetMovie('Shrek'));
+app.get('/:title', async (req, res) => {
+    res.send(await Movies.GetMovie(req.params.title));
 });
 
 app.get('/api/:id', (req, res) => {
