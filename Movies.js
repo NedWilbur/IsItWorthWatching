51f86d2dd5ef34d28a757e7f65580f212ee46872
NedWilbur <Ned612@Gmail.com>
2020-11-02 22:44:50 -0500

    more heroku stuff...

--- Movies.js
+++ Movies.js
@@ -1,13 +1,11 @@
 import {
     MovieDb
-} from 'moviedb-promise'
-const moviedb = new MovieDb('9b0b73c0081cd92f6455b2bace97c1ca')
+} from 'moviedb-promise';
+const moviedb = new MovieDb('9b0b73c0081cd92f6455b2bace97c1ca');
 
-async function SearchForMovie(query) {
-    // console.log(`Searching for ${query}`);
-
+async function SearchForMovie(searchQuery) {
     const results = await moviedb.searchMovie({
-        query: query
+        query: searchQuery
     }).then(res => {
         var results = res.results;
         results.sort((a, b) => b.vote_count - a.vote_count)
